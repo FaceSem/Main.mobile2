eea7b95da531b1a3dfe3aa597144ae69e7de27fc
FaceSem <>
2019-03-09 11:19:22 +0200

    header

--- js/script.js
+++ js/script.js
@@ -204,7 +204,7 @@ $(document).ready(function () {
     if ($(".desktop").css("display") == "none") {
         bubblecount = ($(".section-1").width() / 10);
         topPos = 12;
-        randomMax = 135;
+        randomMax = 150;
         maxLeft = 80;
         posName = "vw";
         
